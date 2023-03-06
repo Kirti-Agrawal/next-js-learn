dd58a81d4582e0059d2da764503bde91676a2662
Kirti-Agrawal <121790445+Kirti-Agrawal@users.noreply.github.com>
2023-03-06 13:19:53 +0530

    Initial Commit

---
+++ components/layout.js
@@ -0,0 +1,61 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+
+export const siteTitle = 'Next.js Sample Website';
+const name = ' Kirti Sister';
+export default function Layout({ children, home }) {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel='icon' href='/favicon.ico'></link>
+                <meta name='description'
+                    content="Learn how to build a personal website using Next.js" />
+                <meta property='og:image'
+                    content={`https://og-image.vercel.app/${encodeURI(
+                        siteTitle,
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`} />
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <header className={styles.header}>
+                {home ? (
+                    <>
+                        <Image
+                            priority
+                            src="/images/Flower Soft Wallpaper.jpeg"
+                            alt="Beautiful Flower Background"
+                            width={144}
+                            height={144}
+                            className={utilStyles.borderCircle} />
+                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                    </>
+                ) : (
+                    <>
+                        <Link href="/">
+                            <Image
+                                priority
+                                src="/images/Flower Soft Wallpaper.jpeg"
+                                alt="Beautiful Flower Background"
+                                width={108}
+                                height={108}
+                                className={utilStyles.borderCircle}
+                            />
+                        </Link>
+                        <h2 className={utilStyles.headingLg}>
+                            <Link href="/" className={utilStyles.colorInherit}>{name}</Link>
+                        </h2>
+                    </>
+                )}
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/"> - Back To Home</Link>
+                </div>
+            )}
+        </div>
+    );
+}
